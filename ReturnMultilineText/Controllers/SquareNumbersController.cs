3d2560ecbb9d9f563ed0c45a9907c6a882bbfbba
Declan Keogh <declankeogh01@gmail.com>
2017-11-07 17:14:14 +0100

    Add project files.

---
+++ ReturnMultilineText/Controllers/SquareNumbersController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web;
+
+namespace ReturnMultilineText.Controllers
+{
+    public class SquareNumbersController : ApiController
+    {
+
+        [HttpPost]
+        public List<int> SquareNums(string txtString)
+        {
+            List<string> caseList = txtString.Split().ToList();
+            List<int> intList = caseList.ConvertAll(int.Parse);
+            List<int> ans = new List<int>();
+
+            for (var i = 0; i < intList.Count; i++)
+            {
+                ans.Add((intList[i] * intList[i]));
+            }
+            return ans;
+        }
+    }
+}
